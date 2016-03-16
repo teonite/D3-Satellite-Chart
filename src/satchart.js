07f604ae1d99b8d392fecf6214c45f5d0dd136c6
Jacek Chmielewski <jchmielewski@teonite.com>
2016-03-16 09:12:09 +0100

    simple inner & outer sun TODO: sectors

--- src/satchart.js
+++ src/satchart.js
@@ -1,5 +1,60 @@
 export class SatChart {
-  constructor() {
-    console.log('SatChart constructor');
+
+  constructor(element, {
+    width = 600,
+    height = 300,
+    valueRange = [1, 10]
+    }) {
+
+    // config
+    const outerSunRadius = height / 10;
+    const innerSunRadius = outerSunRadius / 2;
+
+    this.element = element;
+    this.config = {
+      width,
+      height,
+      valueRange,
+      outerSunRadius,
+      innerSunRadius
+    };
+
+    // draw chart
+    this.init();
+
+  }
+
+  init() {
+    this.svg = d3.select(this.element)
+      .append('svg')
+      .attr('width', this.config.width)
+      .attr('height', this.config.height);
+
+    this.outerSun = this.svg.append('g')
+      .attr('class', 'sun outer')
+      .append('circle')
+      .attr({
+        cx: this.config.width / 2,
+        cy: this.config.height / 2,
+        r: this.config.outerSunRadius,
+        stroke: 'black',
+        'stroke-width': 2,
+        fill: 'white'
+      });
+
+    this.innerSun = this.svg.append('g')
+      .attr('class', 'sun outer')
+      .append('circle')
+      .attr({
+        cx: this.config.width / 2,
+        cy: this.config.height / 2,
+        r: this.config.innerSunRadius,
+        stroke: 'black',
+        'stroke-width': 2,
+        fill: 'white'
+      });
+
+
+
   }
 }
