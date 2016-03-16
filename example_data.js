ad88386a455aa7240afc18d2d2bb15f96b633e52
Jacek Chmielewski <jchmielewski@teonite.com>
2016-03-16 10:12:00 +0100

    outer sun sectors

---
+++ example_data.js
@@ -0,0 +1,102 @@
+var data = {
+  label: 'A',
+  value: 9,
+  satellites: [
+    {
+      label: 'Sat1',
+      value: 1,
+      satellites: [
+        {
+          label: 'Moon1',
+          value: 1
+        },
+        {
+          label: 'Moon2',
+          value: 3
+        },
+        {
+          label: 'Moon3',
+          value: 6
+        },
+        {
+          label: 'Moon4',
+          value: 10
+        }
+      ]
+    },
+    {
+      label: 'Sat2',
+      value: 3,
+      satellites: [
+        {
+          label: 'Moon5',
+          value: 1
+        },
+        {
+          label: 'Moon6',
+          value: 5
+        },
+        {
+          label: 'Moon7',
+          value: 10
+        }
+      ]
+    },
+    {
+      label: 'Sat3',
+      value: 6,
+      satellites: [
+        {
+          label: 'Moon8',
+          value: 1
+        },
+        {
+          label: 'Moon9',
+          value: 3
+        },
+        {
+          label: 'Moon10',
+          value: 6
+        },
+        {
+          label: 'Moon11',
+          value: 10
+        },
+        {
+          label: 'Moon12',
+          value: 2
+        },
+        {
+          label: 'Moon13',
+          value: 4
+        },
+        {
+          label: 'Moon14',
+          value: 7
+        },
+        {
+          label: 'Moon15',
+          value: 9
+        }
+      ]
+    },
+    {
+      label: 'Sat4',
+      value: 10,
+      satellites: [
+        {
+          label: 'Moon16',
+          value: 1
+        },
+        {
+          label: 'Moon17',
+          value: 5
+        },
+        {
+          label: 'Moon18',
+          value: 10
+        }
+      ]
+    }
+  ]
+};
--- src/satchart.js
+++ src/satchart.js
@@ -1,9 +1,10 @@
 export class SatChart {
 
-  constructor(element, {
+  constructor(element, data, {
     width = 600,
     height = 300,
-    valueRange = [1, 10]
+    valueRange = [1, 10],
+    strokeWidth = 2
     }) {
 
     // config
@@ -11,8 +12,13 @@ export class SatChart {
     const innerSunRadius = outerSunRadius / 2;
 
     this.element = element;
+    this.data = data;
+    const [cx, cy] = [width / 2, height / 2];
     this.config = {
       width,
+      cx,
+      cy,
+      strokeWidth,
       height,
       valueRange,
       outerSunRadius,
@@ -25,23 +31,38 @@ export class SatChart {
   }
 
   init() {
+    // svg
     this.svg = d3.select(this.element)
       .append('svg')
       .attr('width', this.config.width)
       .attr('height', this.config.height);
 
+    // outer sun
     this.outerSun = this.svg.append('g')
-      .attr('class', 'sun outer')
+      .attr('class', 'sun outer');
+    this.outerSun
       .append('circle')
       .attr({
-        cx: this.config.width / 2,
-        cy: this.config.height / 2,
+        cx: this.config.cx,
+        cy: this.config.cy,
         r: this.config.outerSunRadius,
         stroke: 'black',
-        'stroke-width': 2,
+        'stroke-width': this.config.strokeWidth,
         fill: 'white'
       });
 
+    this.outerSun.selectAll('line')
+      .data(this.data.satellites)
+      .enter()
+      .append('line')
+      .attr('x1', (d, i) => this.config.cx + this.config.outerSunRadius * Math.sin(i * 2*Math.PI / this.data.satellites.length))
+      .attr('y1', (d, i) => this.config.cy + this.config.outerSunRadius * Math.cos(i * 2*Math.PI / this.data.satellites.length))
+      .attr('x2', this.config.cx)
+      .attr('y2', this.config.cy)
+      .attr('stroke', 'black')
+      .attr('stroke-width', this.config.strokeWidth);
+
+    // inner sun
     this.innerSun = this.svg.append('g')
       .attr('class', 'sun outer')
       .append('circle')
@@ -50,11 +71,9 @@ export class SatChart {
         cy: this.config.height / 2,
         r: this.config.innerSunRadius,
         stroke: 'black',
-        'stroke-width': 2,
+        'stroke-width': this.config.strokeWidth,
         fill: 'white'
       });
-
-
 
   }
 }
