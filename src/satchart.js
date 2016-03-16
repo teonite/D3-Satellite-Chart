ffd145827d50ca45c7721fe82979529d2f207dd9
Jacek Chmielewski <jchmielewski@teonite.com>
2016-03-16 12:47:30 +0100

    layout + planets

--- src/satchart.js
+++ src/satchart.js
@@ -4,12 +4,17 @@ export class SatChart {
     width = 600,
     height = 300,
     valueRange = [1, 10],
-    strokeWidth = 2
+    strokeWidth = 2,
+    distanceRatio = 4 // sun-to-planets / planets-to-moons
     }) {
 
     // config
     const outerSunRadius = height / 10;
     const innerSunRadius = outerSunRadius / 2;
+    const planetRadius = height / 20;
+    const moonRadius = height / 40;
+    const sunToPlanet = (height / 2 - moonRadius) / (1 + 1 / distanceRatio);
+    const planetToMoon = sunToPlanet / distanceRatio;
 
     this.element = element;
     this.data = data;
@@ -18,6 +23,10 @@ export class SatChart {
       width,
       cx,
       cy,
+      planetRadius,
+      moonRadius,
+      sunToPlanet,
+      planetToMoon,
       strokeWidth,
       height,
       valueRange,
@@ -31,6 +40,9 @@ export class SatChart {
   }
 
   init() {
+
+    this.computeLayout();
+
     // svg
     this.svg = d3.select(this.element)
       .append('svg')
@@ -43,8 +55,8 @@ export class SatChart {
     this.outerSun
       .append('circle')
       .attr({
-        cx: this.config.cx,
-        cy: this.config.cy,
+        cx: this.data.position.x,
+        cy: this.data.position.y,
         r: this.config.outerSunRadius,
         stroke: 'black',
         'stroke-width': this.config.strokeWidth,
@@ -67,13 +79,47 @@ export class SatChart {
       .attr('class', 'sun outer')
       .append('circle')
       .attr({
-        cx: this.config.width / 2,
-        cy: this.config.height / 2,
+        cx: this.data.position.x,
+        cy: this.data.position.y,
         r: this.config.innerSunRadius,
         stroke: 'black',
         'stroke-width': this.config.strokeWidth,
         fill: 'white'
       });
 
+    // planets
+    this.planets = this.svg.append('g')
+      .attr('class', 'planets');
+
+    this.planets.selectAll('circle')
+      .data(this.data.satellites)
+      .enter()
+      .append('circle')
+      .attr({
+        cx: (d) => d.position.x,
+        cy: (d) => d.position.y,
+        r: this.config.planetRadius,
+        stroke: 'black',
+        'stroke-width': this.config.strokeWidth,
+        fill: 'white'
+      })
+  }
+
+  computeLayout() {
+
+    // sun position
+    this.data.position = {
+      x: this.config.cx,
+      y: this.config.cy
+    };
+
+    const planetAngle = 2 * Math.PI / this.data.satellites.length;
+    this.data.satellites.forEach((planet, index) => {
+      planet.position = {
+        x: this.config.cx + this.config.sunToPlanet * Math.sin(planetAngle * (index + 0.5)),
+        y: this.config.cy + this.config.sunToPlanet * Math.cos(planetAngle * (index + 0.5))
+      };
+      // TODO: moons
+    })
   }
 }
